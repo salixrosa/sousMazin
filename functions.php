b4492f82505aa4d7baa1de15eea9febf4b7f0262
salixrosa <willow.rose.sells@gmail.com>
2014-05-07 23:16:11 -0500

    update to content filter to make content compatible with highlight.js when done correctly

--- functions.php
+++ functions.php
@@ -11,16 +11,19 @@ function my_custom_more_text() {
 	return "<a href='" . get_permalink() . "'><div id='more'>Continue reading " . "\"" . ucwords(get_the_title()) . "\"" ."</a></div>";
 }
 
+
 function more_posts() {
   global $wp_query;
   return $wp_query->current_post + 1 < $wp_query->post_count;
 }
+
 
 function remove_more_link_scroll( $link ) {
 	$link = preg_replace( '|#more-[0-9]+|', '', $link );
 	return $link;
 }
 add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
+
 
 function register_my_menus() {
   register_nav_menus(
@@ -38,5 +41,14 @@ $args['show_home'] = true;
 return $args;
 }
 add_filter( 'wp_page_menu_args', 'home_page_menu_args' );
+
+
+function seesyntax( $text ) {
+  $preblocks = array("<pre class='portrait'>", "<pre class='landscape'>", "</pre>");
+  $highlightblocks = array("<pre><code class='portrait'>", "<pre><code class='landscape'>", "</code></pre>");
+  $text = str_replace($preblocks, $highlightblocks, $text);
+  return $text;
+}
+add_filter('the_content', 'seesyntax');
 
 ?>
