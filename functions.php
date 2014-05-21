271f536a0dc675f1ca53148403a6ed361b080377
salixrosa <willow.rose.sells@gmail.com>
2014-05-21 11:54:28 -0500

    NEW FEATURE - first working chtulusyntaxhelper()

--- functions.php
+++ functions.php
@@ -42,13 +42,69 @@ return $args;
 }
 add_filter( 'wp_page_menu_args', 'home_page_menu_args' );
 
+function cthulusyntaxhelper($posttext) {
 
-function seesyntax( $text ) {
-  $preblocks = array("<pre class='portrait'>", "<pre class='landscape'>", "</pre>");
-  $highlightblocks = array("<pre class='portrait'><code class='portrait'>", "<pre class='landscape'><code class='landscape'>", "</code></pre>");
-  $text = str_replace($preblocks, $highlightblocks, $text);
-  return $text;
+  preg_match_all('@<pre(?:\sclass\=.*?)?><code(?:\sclass\=.*?)?>@iU', $posttext, $openingregex, PREG_OFFSET_CAPTURE);
+  preg_match_all('@</code>(?:\s*\n*)*</pre>@i', $posttext, $closingregex, PREG_OFFSET_CAPTURE);
+
+  $numberofopenblocks = count($openingregex[0]);
+  $numberofclosedblocks = count($closingregex[0]);
+
+  if ($numberofopenblocks !== $numberofclosedblocks OR $numberofopenblocks == 0){
+    return "ERROR" . $posttext;
+  }
+  else {
+   $i = 1;
+   while($i <= $numberofopenblocks){
+     $offsetkey = $openingregex[0][$i-1][1];
+     $orderedarray[$offsetkey] = array($openingregex[0][$i-1][0], 1);
+     $i++;
+   }
+   $i = 1;
+   while($i <= $numberofclosedblocks){
+     $offsetkey = $closingregex[0][$i-1][1];
+     $orderedarray[$offsetkey] = array($closingregex[0][$i-1][0],-1);
+     $i++;
+   }
+
+   ksort($orderedarray);
+
+   $blocks = array();
+   $blockcounter = 0;
+   $blocknumber = 1;
+
+   foreach($orderedarray as $key => $match){
+      $blockcounter = $blockcounter + $match[1];
+      if($blockcounter == 1 && $match[1] == 1){
+       $blocks[$blocknumber][0] = $key+strlen($orderedarray[$key][0]);
+      }
+      if($blockcounter == 0 && $match[1] == -1) {
+        $blocks[$blocknumber][1] = $key;
+        $blocknumber++;
+      }
+    }
+
+    $arraytext = array();
+    foreach($blocks as $key => $block){
+     $blocks[$key][2] = $blocks[$key][1]-$blocks[$key][0];
+     $blocks[$key][3] = htmlspecialchars(htmlspecialchars_decode(substr($posttext,$blocks[$key][0],$blocks[$key][2])));
+
+     $arraytext[] = substr($posttext, 0, $blocks[$key][0]);
+     $arraytext[] = $blocks[$key][3];
+     if(empty($blocks[($key+1)])){
+      $arraytext[] = substr($posttext, $blocks[$key][1]);
+     }
+     else{
+      $arraytext[] = substr($posttext, $blocks[$key][1], $blocks[$key][0]);
+     }
+    }
+
+    $newtext = implode($arraytext);
+
+    return $newtext;
+  }
+
 }
-add_filter('the_content', 'seesyntax');
+add_filter( 'content_save_pre', 'cthulusyntaxhelper');
 
 ?>
